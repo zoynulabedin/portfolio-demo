6413430b855f5303eda947da5f40c9acebdccbdc
zoynulabedin <trustcodernet@gmail.com>
2025-05-31 11:08:07 +0600

    from test1

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -80,7 +80,7 @@ export default function Index() {
               onClick={handleNavClick}
               className="text-gray-700 hover:text-purple-600 transition"
             >
-              About menu from test1
+              About menu from test11
             </a>
             <a
               href="#projects"
